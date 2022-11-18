1b6b2d4290e9e1367d6234aba41cc5af34a8a928
Guilherme Ramos <guilhermeramos2001@gmail.com>
2022-11-17 21:58:42 -0300

    Update automacao.py

--- Aula - 9/automacao.py
+++ Aula - 9/automacao.py
@@ -1,3 +1,16 @@
-from os import system 
+from requests import get
 from bs4 import BeautifulSoup
-from
+from os import system
+system('cls')
+
+url = 'https://economia.uol.com.br/'
+response = get(url) #aqui o requests le a página e trás o html
+print(response)
+html_soup = BeautifulSoup(response.text,"html.parser")
+#print(html_soup)
+#cotacao = html_soup.find_all('span', class_ ='Value bra')
+cotacao = html_soup.find_all('a', class_ ='subtituloGrafico subtituloGraficoValor')
+print(len(cotacao))
+print(cotacao)
+for valor in cotacao:
+    print(valor.text)
